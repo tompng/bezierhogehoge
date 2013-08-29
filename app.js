0a9dce47c25b669bd84e777eef6f4a21e387367c
tompng <tomoyapenguin@gmail.com>
2013-08-30 01:18:22 +0900

    move!

--- app.js
+++ app.js
@@ -20,22 +20,40 @@ var ioserver = io.listen(server);
 
 var objects = {};
 function consume(data){
+  var operations=[];
   for(var i=0;i<data.operations.length;i++){
     var op=data.operations[i];
     if(op.src&&!objects[op.src])continue;
-    if(op.src)delete objects[op.src];
-    for(var id in op.dst){
-      objects[id]=op.dst[id];
+    operations.push(op);
+    switch(op.type){
+      case 'move':{
+        var obj=objects[op.src];
+        for(var j=0;j<obj.length;j++){
+          obj[j].x+=op.dx;
+          obj[j].y+=op.dy;
+        }
+        delete objects[op.src];
+        objects[op.dst]=obj;
+      }break;
+      case 'change':{
+        if(op.src)delete objects[op.src];
+        for(var id in op.dst){
+          objects[id]=op.dst[id];
+        }
+      }break;
     }
   }
+  data.operations=operations;
+  if(operations.length)return true;
 }
 ioserver.on('connection',function(socket){
   console.log('connect');
   socket.on('data',function(data){
-    consume(data);
-    socket.broadcast.emit('data',data);
-    socket.emit('data',data);
+    if(consume(data)){
+      socket.broadcast.emit('data',data);
+      socket.emit('data',data);
+    }
   });
-  socket.emit('data',{operations:[{dst:objects}]})
+  socket.emit('data',{operations:[{type:'change',dst:objects}]})
   socket.on('disconnect',function(){});
 });
--- public/mergebuffer.js
+++ public/mergebuffer.js
@@ -25,32 +25,47 @@ function MergeBuffer(listener){
   this.merged={};
   this.unmerged={};
 }
-MergeBuffer.prototype.merge=function(data,buffer,forward){
-  console.log('merge',data,buffer);
-  if(data){
-    for(var i=0;i<data.operations.length;i++){
-      var op=data.operations[i];
-      if(!op.src||this.merged[op.src]){
-        if(op.src)delete this.merged[op.src];
-        for(var id in op.dst){
-          this.merged[id]=op.dst[id];
+function consume(objects,data,del){
+  for(var i=0;i<data.operations.length;i++){
+    var op=data.operations[i];
+    if(op.src&&!objects[op.src])continue;
+    switch(op.type){
+      case 'move':{
+        var obj=objects[op.src];
+        var obj2=[];
+        for(var j=0;j<obj.length;j++){
+          var p=obj[j];
+          obj2[j]={
+            x:p.x+op.dx,y:p.y+op.dy,
+            dx:p.dx,dy:p.dy,
+            ln:p.ln,lp:p.lp
+          };
         }
-      }
+        if(del)delete objects[op.src];
+        else objects[op.src]=null;
+        objects[op.dst]=obj2;
+      }break;
+      case 'change':{
+        if(op.src){
+          if(del)delete objects[op.src];
+          else objects[op.src]=null;
+        }
+        for(var id in op.dst){
+          objects[id]=op.dst[id];
+        }
+      }break;
     }
+  }
+}
+MergeBuffer.prototype.merge=function(data,buffer,forward){
+  //console.log('merge',data,buffer);
+  if(data){
+    consume(this.merged,data,true);
   }
   this.unmerged={};
   this.unmerged.__proto__=this.merged;
   for(var i=0;i<buffer.length;i++){
-    for(var j=0;j<buffer[i].operations.length;j++){
-      var op=buffer[i].operations[j];
-      if(!op.src||this.unmerged[op.src]){
-        delete this.unmerged[op.src];
-        this.unmerged[op.src]=null;
-        for(var id in op.dst){
-          this.unmerged[id]=op.dst[id]
-        }
-      }
-    }
+    consume(this.unmerged,buffer[i],false);
   }
   this.listener.update(this.unmerged);
 }
